ce4511a0bd706859c55bd15e8ab20fe7d4d4a724
Aritra Bhaduri <92646038+CodingwithMe123@users.noreply.github.com>
2022-07-17 21:00:39 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error
+
+diabetes = datasets.load_diabetes()
+
+# take the entire third coloumn in diabetes_X
+# diabetes_X = diabetes.data[:, np.newaxis, 2]
+diabetes_X = diabetes.data
+
+# use last 30 elements for training
+diabetes_X_train = diabetes_X[:-30]
+# diabetes_X_train = diabetes_X
+
+# use first 30 elements for testing
+diabetes_X_test = diabetes_X[-30:]
+
+diabetes_Y_train = diabetes.target[:-30]
+# diabetes_Y_train = diabetes.target
+diabetes_Y_test = diabetes.target[-30:]
+
+model = linear_model.LinearRegression()
+model.fit(diabetes_X_train, diabetes_Y_train)
+# testing model now
+diabetes_Y_predict = model.predict(diabetes_X_test)
+
+print("Mean squared error is: ", mean_squared_error(diabetes_Y_test, diabetes_Y_predict))
+print("Weights : ", model.coef_)
+print("Intercept : ", model.intercept_)
+
+# plt.scatter(diabetes_X_test, diabetes_Y_test)
+# plt.plot(diabetes_X_test, diabetes_Y_predict)
+# plt.show()
